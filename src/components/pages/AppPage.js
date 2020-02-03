d8329354355d2f2fe959e5aa5e9efb2b32da10bd
Maximilien <maximilien.raes@itctc.be>
2020-02-03 16:13:40 +0100

    remove un used imports

--- src/components/pages/AppPage.js
+++ src/components/pages/AppPage.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { useSnackbar } from 'notistack';
 import { useEffect } from 'react';
 import { ServiceLocatorContext } from '../context';
-import { DataTable, TextField, ComboBox, Dialog } from '../ui-kit';
+import { DataTable } from '../ui-kit';
 import { Grid } from '@material-ui/core';
 import ActionButton from '../ui-kit/buttons/ActionButton/ActionButton';
 
